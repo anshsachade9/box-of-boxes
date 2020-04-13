3e1cb453f355186cd12800d1b3e3e69dfdf6b2a0
anshsachade9 <63061552+anshsachade9@users.noreply.github.com>
2020-04-13 21:18:49 +0530

    Add files via upload

---
+++ myOwnLibrary.js
@@ -0,0 +1,28 @@
+function isTouching(Object1,Object2){
+    if(Object1.x - Object2.x<Object2.width/2 +Object1.width/2
+      && Object2.x -Object1.x<Object2.width/2 + Object1.width/2
+      && Object1.y- Object2.y<Object2.height/2 +Object1.height/2
+      && Object2.y -Object1.y<Object2.height/2 + Object1.height/2 ){
+  
+  return true;
+      }
+      else{
+       return false;
+  
+      }
+  }
+  function bounceOff(object1,object2){
+    if(Object1.x - Object2.x<Object2.width/2 +Object1.width/2
+      && Object2.x -Object1.x<Object2.width/2 + Object1.width/2){
+     object1.velocityX = object1.velocityX * (-1);
+     object2.velocityX = object2.velocityX*(-1);    
+      }
+  if(Object1.y- Object2.y<Object2.height/2 +Object1.height/2
+    && Object2.y -Object1.y<Object2.height/2 + Object1.height/2){
+      object1.velocityY = object1.velocityY * (-1);
+      object2.velocityY = object2.velocityY*(-1); 
+    }
+  }
+  
+  
+  
